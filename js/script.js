ea50ed94cb0d447f55830ebfd9a066f7831b5245
Robert-Stefan <robertboss05@hotmail.it>
2021-04-20 15:01:56 +0200

    Add h1 and bonus

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ * Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+//console.log('Vue', Vue);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Le comiche avventure di Brain nel corpo di Lillo',
+        image: 'meme.jpg'
+    }
+});
